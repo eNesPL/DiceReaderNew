6e9464155e07f3fadb8e85f0c305705c539bd7b2
eNesPL <36967538+eNesPL@users.noreply.github.com>
2021-07-17 22:14:48 +0200

    RestApi Podstawy2

--- RestApiServer.py
+++ RestApiServer.py
@@ -1,8 +1,6 @@
-import socket
 from wsgiref.simple_server import make_server
 
 import falcon
-
 
 # Falcon follows the REST architectural style, meaning (among
 # other things) that you think in terms of resources and state
@@ -20,16 +18,27 @@ class ThingsResource:
                      '\n'
                      '    ~ Immanuel Kant\n\n')
 
+
 class GetDice:
     def on_get(self, req, resp):
         from DiceReader import readDice
         resp.status = falcon.HTTP_200
         resp.text = str(readDice())
 
+
+class GetConnection:
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.text = "connected"
+
+
 global ui
+
+
 def RestApiGetUi(d):
     global ui
     ui = d
+
 
 # falcon.App instances are callable WSGI apps
 # in larger applications the app is created in a separate file
@@ -38,18 +47,21 @@ app = falcon.App()
 # Resources are represented by long-lived class instances
 config = Config()
 dice = GetDice()
+connection = GetConnection()
+
+
 # things will handle all requests to the '/things' URL path
 
 
 def AddRoutes():
     app.add_route('/config', config)
     app.add_route('/getdice', dice)
-    app.add_route('/getuseracrtion',ui)
-
-
+    app.add_route('/getuseracrtion', ui)
+    app.add_route('/getConnection', connection)
 
 
 def RestApiServer():
     with make_server('', 8000, app) as httpd:
         print('Serving on port 8000...')
         httpd.serve_forever()
+
